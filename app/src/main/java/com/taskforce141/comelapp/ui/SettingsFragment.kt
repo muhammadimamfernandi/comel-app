740204fe3f25b455f62fe266c90b11604449c4c5
muhammadimamfernandi <fernandi.bkw@gmail.com>
2022-05-18 22:25:19 +0700

    gradle dan sedikit perbaikan

--- app/src/main/java/com/taskforce141/comelapp/ui/SettingsFragment.kt
+++ app/src/main/java/com/taskforce141/comelapp/ui/SettingsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.taskforce141.comelapp.R
 import com.taskforce141.comelapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
@@ -19,6 +21,20 @@ class SettingsFragment : Fragment() {
         binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.homeNavbar.setOnClickListener{
+            it.findNavController().navigate(
+                R.id.action_settingsFragment_to_homeFragment
+            )
+        }
+
+        binding.communityNavbar.setOnClickListener{
+            it.findNavController().navigate(
+                R.id.action_settingsFragment_to_communityFragment
+            )
+        }
     }
 
 }
