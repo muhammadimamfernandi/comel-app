63d4be27e8fd53a660a066340fd86745ccc1d14d
asus <yaumilramkhadani@gmail.com>
2022-05-18 15:14:46 -0700

    inisialisasi firebase auth register

--- app/src/main/java/com/taskforce141/comelapp/ui/RegisterFragment.kt
+++ app/src/main/java/com/taskforce141/comelapp/ui/RegisterFragment.kt
@@ -1,16 +1,23 @@
 package com.taskforce141.comelapp.ui
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.taskforce141.comelapp.R
 import com.taskforce141.comelapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    //view binding
     private lateinit var binding: FragmentRegisterBinding
+    //firebase initialize
+    private lateinit var auth: FirebaseAuth
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +35,6 @@ class RegisterFragment : Fragment() {
                 R.id.action_registerFragment_to_landingFragment
             )
         }
-
         binding.registerButton.setOnClickListener{
             it.findNavController().navigate(
                 R.id.action_registerFragment_to_homeFragment
